c686d09e1556bdb80cdfd10e13fc495b839554a6
Pagenchen <1443251462@qq.com>
2020-11-19 10:42:20 +0800

    欢迎界面，学习界面小更新

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,11 +1,18 @@
-﻿layui.use(['jquery', 'util'], function () {
+﻿layui.use(['form','jquery', 'util', 'miniTab'], function () {
     var $ = layui.jquery,
-        util = layui.util;
+        form = layui.form,
+        util = layui.util,
+        miniTab = layui.miniTab;
+
+    miniTab.listen();
+
+
     $(window).load(function () {
         $("#loading").fadeOut(500);
         new WOW().init();
-    })
-    util.fixbar();;
+    });
+    util.fixbar();
+
     $('.next').click(function () {
         $('html,body').animate({
             scrollTop: $('#section1').outerHeight() + 1
@@ -17,12 +24,12 @@
             $(this).removeClass('menu_open').addClass('menu_close');
             //open
             $('#navgation').removeClass('navgation_close').addClass('navgation_open');
-            $(this).attr({ 'data-mark': "true" });
+            $(this).attr({'data-mark': "true"});
         } else {
             $(this).removeClass('menu_close').addClass('menu_open');
             //close
             $('#navgation').removeClass('navgation_open').addClass('navgation_close');
-            $(this).attr({ 'data-mark': "false" });
+            $(this).attr({'data-mark': "false"});
         }
     });
 
